ff750c0a5aa966b160d28fc53c68d32563fd4458
piyushd3112 <dhonglep@gmail.com>
2024-08-06 17:11:18 +0530

    completed user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -24,6 +24,8 @@ export default function Profile() {
 
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
 const dispatch = useDispatch();
 
@@ -110,6 +112,7 @@ const dispatch = useDispatch();
   const handleSignOut = async () => {
     try {
       dispatch (signOutUserStart());
+      
       const res = await fetch('/api/auth/signout');
       const data = await res.json();
       if (data.success === false) {
@@ -121,6 +124,25 @@ const dispatch = useDispatch();
       dispatch(deleteUserFailure(data.message));
     }
   }
+
+
+ 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -154,7 +176,8 @@ const dispatch = useDispatch();
         <input type="password" placeholder='Password'  id="password" className='border p-3 rounded-lg' />
         
         <button disabled={loading} className='bg-orange-500 text-white rounded-lg uppercase hover:95 p-3 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>  
-        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'} >
           Create Listing
         </Link>
 
@@ -171,6 +194,45 @@ const dispatch = useDispatch();
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p> 
+   
+      <button onClick={handleShowListings} className='text-green-700 w-full'>
+        Show Listings
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>}
+   
     </div>
   )
 }
